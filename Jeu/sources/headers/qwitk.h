6e9655059710ee5f135a98607c24a1544add6ce2
gregaou <gregoire.rit@gmail.com>
2012-11-11 20:58:09 +0100

    Ajout du style

--- Jeu/sources/headers/qwitk.h
+++ Jeu/sources/headers/qwitk.h
@@ -1,22 +1,35 @@
+/**
+ * \file
+ * \author    Miroslav
+ * \version   1.0
+ * \date       12 Novembre 2009
+ * \brief       Définit les modèles de voiture et leur particularités.
+ *
+ * \details    Cette classe surcharge les accesseurs standards du module_voiture pour
+ *                  convenir aux spécificités des différents modèles possibles.
+ */
+
 #ifndef QWITK_H
 #define QWITK_H
 
 #include <QMainWindow>
 
 namespace Ui {
-class QWITK;
+	class QWITK;
 }
 
 class QWITK : public QMainWindow
 {
-    Q_OBJECT
-    
+	Q_OBJECT
+
 public:
-    explicit QWITK(QWidget *parent = 0);
-    ~QWITK();
-    
+	explicit QWITK(QWidget *parent = 0);
+	~QWITK();
+
+public slots:
+
 private:
-    Ui::QWITK *ui;
+	Ui::QWITK *ui;
 };
 
 #endif // QWITK_H
