7a11ba838f265affc83aa863841305a32d99afdb
gregaou <gregoire.rit@gmail.com>
2012-11-11 21:14:23 +0100

    Documentation de qwitk.h

--- Jeu/sources/headers/qwitk.h
+++ Jeu/sources/headers/qwitk.h
@@ -1,12 +1,9 @@
 /**
- * \file
- * \author    Miroslav
+ * \file			qwitk.h
+ * \brief			Fenêtre principale
+ * \author    Société de Développement de Projets Informatique
  * \version   1.0
- * \date       12 Novembre 2009
- * \brief       Définit les modèles de voiture et leur particularités.
- *
- * \details    Cette classe surcharge les accesseurs standards du module_voiture pour
- *                  convenir aux spécificités des différents modèles possibles.
+ * \date      11/11/12
  */
 
 #ifndef QWITK_H
@@ -14,22 +11,46 @@
 
 #include <QMainWindow>
 
+/** \namespace Ui
+ *
+ * Espace de nommage pour les
+ * interfaces Qt
+ */
 namespace Ui {
 	class QWITK;
 }
 
+/**
+ * \class QWITK
+ * \brief Classe de la fenêtre principale
+ *
+ *  La classe gère la fenêtre principale
+ */
 class QWITK : public QMainWindow
 {
 	Q_OBJECT
 
 public:
+	/**
+		*  \brief Constructeur
+		*	 \fn    explicit QWITK (QWidget *parent = 0)
+		*  \param parent Définit le parent de la fenêtre
+		*
+		*  Constructeur de la classe QWITK
+		*/
 	explicit QWITK(QWidget *parent = 0);
+	/**
+		*  \brief Constructeur
+		*	 \fn    ~QWITK ()
+		*
+		*  Destructeur de la classe QWITK
+		*/
 	~QWITK();
 
 public slots:
 
 private:
-	Ui::QWITK *ui;
+	Ui::QWITK *ui; /**< Interface graphique liée à la classe */
 };
 
 #endif // QWITK_H
